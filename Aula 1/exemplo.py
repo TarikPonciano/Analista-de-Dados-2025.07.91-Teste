d68634b88d0a95ba38660bf7bd7238ac9a24b2d2
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-01-05 20:20:22 -0300

    Aula 1

---
+++ Aula 1/exemplo.py
@@ -0,0 +1,16 @@
+num1 = 20
+num2 = 35
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f'''
+Resultados:
+
+      Soma: {soma}
+      Subtração: {subtracao}
+      Multiplicação: {multiplicacao}
+      Divisão: {divisao}
+''')
---
+++ Aula 1/primeira_aula.py
@@ -0,0 +1,33 @@
+# Imprimir "Olá, Maria!"
+# Imprimir "Seja bem vindo, Maria!"
+
+#nome_da_variavel Snake Case
+#nomeDaVariavel Camel Case
+
+# nome_cliente = "Maria"
+
+# print(f"Olá, {nome_cliente}!")
+# print(f"Seja bem vindo, {nome_cliente}!")
+
+# Crie um programa que imprime na tela o seguinte texto:
+# Olá, meu nome é {nome}! Tenho {idade} anos e {altura} metros!
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+#Tipos de Dados Primitivos
+# str #Texto
+# bool #Lógico
+# int #Inteiro
+# float #Decimal
+
+
+print(f"Olá, meu nome é {nome}! Tenho {idade} anos e {altura} metros!")
+
+# Imprima na tela a data de nascimento dessa pessoa com a mensagem a seguir. Considere o ano atual como 2025. Ignore se a pessoa já fez aniversário ou não:
+# "Nasci no ano {ano}!"
+data_de_nascimento = 2025 - idade
+
+print(f"Nasci no ano {data_de_nascimento}!")
+
